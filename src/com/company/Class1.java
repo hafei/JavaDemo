7909004bdd3985407f66d4ef7b9f953ad1054a81
Sean <466374952@qq.com>
2016-12-09 00:06:24 +0800

    1

---
+++ src/com/company/Class1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by Sean on 2016/12/8.
+ */
+
+public class Class1 {
+
+    public void HelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public void Array(){
+        int[] arr = new int[]{3,5,1,7};
+        for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+        }
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+/*        System.out.println("111");
+
+        //循环
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+        String str = new String();
+        str = "str1";
+
+        String str3 = new String();
+
+        str3 = str.concat("11");
+        System.out.println(str3);*/
+
+        new Class1().HelloWorld();
+        new Class1().Array();
+    }
+}
+
